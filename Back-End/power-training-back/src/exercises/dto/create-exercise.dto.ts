b24598f90336de1b3cc71b3db8e39d7b8fd63681
Agustin Taborda <agustinbarto77@gmail.com>
2024-09-20 10:02:17 +1000

    Crud exercise

--- Back-End/power-training-back/src/exercises/dto/create-exercise.dto.ts
+++ Back-End/power-training-back/src/exercises/dto/create-exercise.dto.ts
@@ -1,1 +1,33 @@
-export class CreateExerciseDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateExerciseDto {
+    @ApiProperty({
+        description: 'Nombre del ejercicio.',
+        example: 'Tricep con Polea'
+    })
+    name: string;
+
+    @ApiProperty({
+        description: 'Descripcion del ejercicio.',
+        example: 'Inclínate ligeramente hacia delante, exhala y empuja la barra hacia abajo utilizando solo los tríceps hasta que la barra llegue a la altura de la cadera y la parte superior del muslo.'
+    })
+    description: string;
+
+    @ApiProperty({
+        description: 'URL del video demostrativo.',
+        example: 'https://www.youtube.com/watch?v=1YSqh2saQi0'
+    })
+    urlVideoExample: string;
+
+    @ApiProperty({
+        description: 'Beneficios del ejercicio',
+        example: 'Trabaja triceps'
+    })
+    benefits: string;
+
+    @ApiProperty({
+        description: 'Palabras clave que identifiquen el ejercicio',
+        example: 'Triceps Polea'
+    })
+    tags: string;
+}
--- Back-End/power-training-back/src/exercises/exercises.service.ts
+++ Back-End/power-training-back/src/exercises/exercises.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
 import { UpdateExerciseDto } from './dto/update-exercise.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,25 +8,38 @@ import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class ExercisesService {
-  constructor(@InjectRepository(ExerciseEntity) private exerciseRepository: Repository<ExerciseEntity>) {}
+  constructor(@InjectRepository(ExerciseEntity) private exercisesRepository: Repository<ExerciseEntity>) {}
   async create(createExerciseDto: CreateExerciseDto) {
-    const exercise = this.exerciseRepository.create(createExerciseDto)
-    return await this.exerciseRepository.save(exercise)
+    const exercise = this.exercisesRepository.create(createExerciseDto)
+    return await this.exercisesRepository.save(exercise)
   }
 
  async findAll() {
-    return await this.exerciseRepository.find();
+    return await this.exercisesRepository.find();
   }
 
- async findOne(id: uuid) {
-    return await this.exerciseRepository.findOneBy({id});
+ async findOne(id: uuid): Promise<ExerciseEntity> {
+    const exercise = await this.exercisesRepository.findOne({ where: { id } });
+    if (!exercise) {
+      throw new NotFoundException(`Exercise with ID ${id} not found`);
+    }
+    return exercise;
   }
 
-  async update(id: uuid, updateExerciseDto: UpdateExerciseDto) {
-    return await this.exerciseRepository.update(id, updateExerciseDto);
+  async update(id: uuid, updateExerciseDto: UpdateExerciseDto): Promise<ExerciseEntity> {
+    const exercise = await this.exercisesRepository.findOne({ where: { id } });
+    if (!exercise) {
+      throw new NotFoundException(`Exercise with ID ${id} not found`);
+    }
+    const updatedExercise = this.exercisesRepository.merge(exercise, updateExerciseDto);
+    return this.exercisesRepository.save(updatedExercise); 
   }
 
   async remove(id: uuid) {
-    return await this.exerciseRepository.delete(id);
+    const exercise = await this.exercisesRepository.findOne({ where: { id } });
+    if (!exercise) {
+      throw new NotFoundException(`Exercise with ID ${id} not found`);
+    }
+    return await this.exercisesRepository.delete(id);
   }
 }
