966146103be31d1f1b0755faf2ae5ff1ab2e06de
Leonardo Baini <bainileonardo@gmail.com>
2024-09-19 22:32:57 -0300

    merge de develop

--- Back-End/power-training-back/src/notifications/notifications.controller.ts
+++ Back-End/power-training-back/src/notifications/notifications.controller.ts
@@ -4,7 +4,7 @@ import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
 import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('notigications')
+@ApiTags('notifications')
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
