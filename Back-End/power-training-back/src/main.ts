7e90c543a8afe41bb8b31327d8ab8409f762cc61
leoramosa <leo.ramos.ari@gmail.com>
2024-09-24 20:36:44 -0300

    google

--- Back-End/power-training-back/src/main.ts
+++ Back-End/power-training-back/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import {SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 //import { config as auth0Config } from './config/auth0.config';
 import { auth } from 'express-openid-connect';
 
@@ -13,52 +13,49 @@ async function bootstrap() {
     clientID: process.env.CLIENT_ID,
     issuerBaseURL: process.env.ISSUER_BASE_URL,
     secret: process.env.SECRET,
-   /* routes: {
+    /* routes: {
       login: '/auth/login',// login: false as false,  // Esto deshabilita la ruta de login por defecto si usas una personalizada
       callback: '/api'  // Define la ruta de callback para la redirección
     }*/
-    
   };
-
 
   const app = await NestFactory.create(AppModule);
   const theme = new SwaggerTheme();
 
   app.use(auth(config));
 
-const options = {
-  explorer: true,
-  customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
-  swaggerOptions: {
-    oauth2RedirectUrl: `${process.env.BASE_URL}/api`, // Configurar redirección
-  }
-};
+  const options = {
+    explorer: true,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+    swaggerOptions: {
+      oauth2RedirectUrl: `${process.env.BASE_URL}/api`, // Configurar redirección
+    },
+  };
 
   const swaggerConfig = new DocumentBuilder()
-                        .setTitle('Api Backend PowerTrainingApp, PF Henry ')
-                        .setDescription('Api con la documentación de cada endpoint disponible en la aplicación')
-                        .setVersion('1.0')
-                        .addOAuth2({
-                          type: 'oauth2',
-                          flows: {
-                            authorizationCode: {
-                              authorizationUrl: `${process.env.BASE_URL}/login`, // URL de login de Auth0
-                              tokenUrl: `${process.env.BASE_URL}/oauth/token`,   // URL del token en Auth0
-                              scopes: {}
-                            }
-                          }
-                        })
-                        .addBearerAuth()
-                        .build();
+    .setTitle('Api Backend PowerTrainingApp, PF Henry ')
+    .setDescription(
+      'Api con la documentación de cada endpoint disponible en la aplicación',
+    )
+    .setVersion('1.0')
+    .addOAuth2({
+      type: 'oauth2',
+      flows: {
+        authorizationCode: {
+          authorizationUrl: `${process.env.BASE_URL}/login`, // URL de login de Auth0
+          tokenUrl: `${process.env.BASE_URL}/oauth/token`, // URL del token en Auth0
+          scopes: {},
+        },
+      },
+    })
+    .addBearerAuth()
+    .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document,options);
- 
-
+  SwaggerModule.setup('api', app, document, options);
 
   //app.use(auth(auth0Config));// ya crea los endpoints necesarios automágicamente
-  app.enableCors()
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(3002);
 }
 bootstrap();
-
