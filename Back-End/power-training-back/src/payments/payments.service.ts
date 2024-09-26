2c6da7a0bd204493cd4ad89ca70ffb4c11d9eb38
Agustin Taborda <agustinbarto77@gmail.com>
2024-09-27 09:15:07 +1000

    mercadopago

--- Back-End/power-training-back/src/payments/payments.service.ts
+++ Back-End/power-training-back/src/payments/payments.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
 
 @Injectable()
 export class PaymentsService {
-  create(createPaymentDto: CreatePaymentDto) {
-    return 'This action adds a new payment';
+  async create(createPaymentDto: CreatePaymentDto) {
+    const client = new MercadoPagoConfig({ accessToken: process.env.YOUR_ACCESS_TOKEN });
+
+    const preference = new Preference(client);
+    
+    const body = {
+      items: [
+        {
+          id: '001',
+          title: 'Suscripcion mensual',
+          unit_price: 1000,
+          quantity: 1,
+          currency_id: 'ARS'
+        }
+      ],
+      back_urls: {
+        "success": "https://3d3a-210-23-154-34.ngrok-free.app/api",
+        "failure": "https://3d3a-210-23-154-34.ngrok-free.app/api",
+        "pending": "https://3d3a-210-23-154-34.ngrok-free.app/api"
+      },
+      auto_return: "approved",
+	  };
+
+    const result = await preference.create( { body } )
+
+// 	Preference.create(preference)
+// 		.then(function (response) {
+// 			res.json({
+// 				id: response.body.id
+// 			});
+// 		}).catch(function (error) {
+// 			console.log(error);
+// 		});
+// });
   }
 
   findAll() {
