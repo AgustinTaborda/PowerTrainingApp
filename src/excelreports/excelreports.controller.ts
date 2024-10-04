11fd8439790f6c036eadb68f53a7f025e922863d
Leonardo Baini <bainileonardo@gmail.com>
2024-10-04 02:34:35 -0300

    prueba de reportes por query ok, a mejorar creando tabla de reportes predefinidos

--- src/excelreports/excelreports.controller.ts
+++ src/excelreports/excelreports.controller.ts
@@ -4,6 +4,7 @@ import { CreateExcelreportDto } from './dto/create-excelreport.dto';
 import { UpdateExcelreportDto } from './dto/update-excelreport.dto';
 import { Response } from 'express';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { queryDto } from './dto/querydto';
 
 @ApiTags('excelreports')
 @Controller('excelreports')
@@ -14,5 +15,13 @@ export class ExcelreportsController {
   @Get('excel')
   async generateExcel(@Res() response: Response) {
     return await this.excelreportsService.generateExcel(response);
+  }
+
+  @ApiOperation({ summary: 'Generate excel report' })
+  @Post('excelGeneric')
+  async  genericReport(@Body() queryInput : queryDto ,@Res() response: Response) {
+    //const query = "SELECT * FROM exercises where tags like '%cardio%'"
+   //  console.log(queryInput.query)
+    return await this.excelreportsService.genericReport(response,queryInput.query);
   }
 }
--- src/excelreports/excelreports.service.ts
+++ src/excelreports/excelreports.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { CreateExcelreportDto } from './dto/create-excelreport.dto';
-import { UpdateExcelreportDto } from './dto/update-excelreport.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExerciseEntity } from 'src/exercises/entities/exercise.entity';
 import { Repository } from 'typeorm';
 import { Workbook } from 'exceljs';
 import { Response } from 'express';
+import { connectionSource } from '../config/typeormConfig';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class ExcelreportsService {
@@ -16,39 +16,63 @@ export class ExcelreportsService {
 
 
   async generateExcel(response: Response): Promise<void> {
-    const data = await this.execisesRepository.find(); // Obtén los datos de la BD
+    const data = await this.execisesRepository.find(); 
 
-    data.forEach((excercise) => {
-      console.log(excercise.urlVideoExample);
-    });
-
-    const workbook = new Workbook();
-    const worksheet = workbook.addWorksheet('Report');
-
-    // Definir encabezados
-    worksheet.columns = [
-      { header: 'Name', key: 'name', width: 10 },
-      { header: 'Description', key: 'description', width: 30 },
-      { header: 'UrlVideoExample', key: 'urlvideoexample', width: 30 },
-      
-    ];
-
-    // Agregar datos a las filas
-    data.forEach((excercise) => {
-      worksheet.addRow({
-        name: excercise.name,
-        description: excercise.description,
-        urlvideoexample: excercise.urlVideoExample
-      });
-    });
-
-    // Configurar la respuesta para descargar el archivo
-    response.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    response.setHeader('Content-Disposition', 'attachment; filename=report.xlsx');
-
-    // Escribir el archivo a la respuesta
-    await workbook.xlsx.write(response);
-    response.end();
+    await this.generateExcelReport(data,response);
+       
   }
+  async genericReport(response: Response,query: string) {
+    
+     
+    try{ 
+    await connectionSource.initialize();
+    const data = await connectionSource.query(query);//"SELECT * FROM exercises where tags like '%cardio%'");
+    console.log(data);
+    await this.generateExcelReport(data,response);
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    finally {
+      await connectionSource.destroy();
+    }
+   
+  //response.send(data);
+  //return "hola"
+  }
+
+
+async  generateExcelReport(data,response) {
+  try{
+  const workbook = new Workbook();
+  const worksheet = workbook.addWorksheet('Report');
+  response.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  response.setHeader('Content-Disposition', 'attachment; filename=report.xlsx');
+
+  if (data.length > 0) {
+
+      // Obtener la metadata
+      const metadata = Object.keys(data[0]); // Claves del primer objeto
+
+      // Definir encabezados dinámicamente
+      worksheet.columns = metadata.map(key => {
+          // Puedes ajustar el ancho según la longitud máxima de los valores si es necesario
+          const width = Math.max(...data.map(item => item[key] ? item[key].toString().length : 0), key.length) + 2;
+          return { header: key.charAt(0).toUpperCase() + key.slice(1), key: key, width: width };
+      });
+
+      // Agregar los datos
+      data.forEach(item => {
+          worksheet.addRow(item); // Agrega cada objeto como una fila
+      });
+  }
+
+  // Guarda el archivo
+  await workbook.xlsx.write(response);
+  response.end();
+  console.log('Reporte generado: reporte.xlsx');
+}catch(error){
+  return  new HttpException('An error occurred while generating the report: '+ error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+}
+}
 
 }
