911901a31760d60f778fa0252a315f49a0efc86d
Leonardo Baini <bainileonardo@gmail.com>
2024-10-09 23:45:34 -0300

    Agregando secure:true en cloudinary

--- src/config/cloudFileManager.ts
+++ src/config/cloudFileManager.ts
@@ -10,7 +10,8 @@ cloudinary.config({
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET,
-    secure: true
+    secure: true,
+    
 });
 
 export const CloudinaryConfig =  {
