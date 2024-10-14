90022fb9d3449cb1ca571d4779585dc28fc812ee
Leonardo Baini <bainileonardo@gmail.com>
2024-10-14 19:57:47 -0300

    Reporte pdf con algo de onda

--- src/pdfreports/pdfreports.controller.ts
+++ src/pdfreports/pdfreports.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/
 import { PdfreportsService } from './pdfreports.service';
 import { Response } from 'express';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PDFToolkitService } from './pdfreports.tutorial';
 
 
 
@@ -9,7 +10,8 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 @Controller('pdfreports')
 export class PdfreportsController {
   constructor(
-    private readonly pdfreportsService: PdfreportsService
+    private readonly pdfreportsService: PdfreportsService,
+    private readonly pdfToolkitService: PDFToolkitService
    
 
   ) {}
@@ -46,31 +48,20 @@ export class PdfreportsController {
   }
   // FUENTE https://firxworx.com/blog/code/nestjs-generate-pdf-with-pdfkit-and-send-to-client/
  
+  @ApiOperation({ summary: 'Customers report PDF, it´s opens in browser directly' })
+  @Get('/pdfExample')
+  async getpdftutorial(
+    @Res() res: Response,
+  ): Promise<void> {
+    const buffer = await this.pdfToolkitService.generarPDF()  
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'inline; filename=example.pdf',
+      'Content-Length': buffer.length,
+    })
 
-/*
-  @Post()
-  create(@Body() createPdfreportDto: CreatePdfreportDto) {
-    return this.pdfreportsService.create(createPdfreportDto);
+    res.end(buffer)
   }
 
-  @Get()
-  findAll() {
-    return this.pdfreportsService.findAll();
-  }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pdfreportsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePdfreportDto: UpdatePdfreportDto) {
-    return this.pdfreportsService.update(+id, updatePdfreportDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pdfreportsService.remove(+id);
-  }
-  */
 }
